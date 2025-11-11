f6371e8e7aa081b8b819b69b4820002b20539f98
andrii2706 <andriipas200017@gmail.com>
2025-11-11 22:13:05 +0200

    lesson-1

--- leson-1/node.js
+++ leson-1/node.js
@@ -1,7 +1,52 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    })
-    
-    server.listen(3000);
+  switch (req.method) {
+    case "GET":
+      switch (req.url) {
+        case "/home":
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          res.end("Home page\n");
+          break;
+        case "/about":
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          res.end("About Page\n");
+          break;
+        default:
+          notFound(res);
+          break;
+      }
+      break;
+    case "POST":
+      switch (req.url) {
+        case "/api/home":
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          res.end("Home page\n");
+          break;
+        case "/api/user":
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          res.end("Create user request \n");
+          break;
+        default:
+          notFound(res);
+          break;
+      }
+      break;
+    default:
+      notFound(res);
+      break;
+  }
+});
+
+function notFound(res) {
+  res.statusCode = 404;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Not Found\n");
+}
+server.listen(3000, "localhost", () => {
+  console.log(`Server listening http://localhost:3000`);
+});
